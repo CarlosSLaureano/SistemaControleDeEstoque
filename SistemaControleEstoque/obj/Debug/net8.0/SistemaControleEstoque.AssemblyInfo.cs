e05a9b20755abef6fb70ba72824776cc389eced5
CarlosSLaureano <carloslaureano1@icloud.com>
2025-05-01 13:07:25 -0300

    Exclusão vercel.json

--- SistemaControleEstoque/obj/Debug/net8.0/SistemaControleEstoque.AssemblyInfo.cs
+++ SistemaControleEstoque/obj/Debug/net8.0/SistemaControleEstoque.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaControleEstoque")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10852cc8d093e64e25b443e748b2e4a999a5ee01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+595b7997620df3c45b29d063b5d7af33e2597511")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaControleEstoque")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaControleEstoque")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
